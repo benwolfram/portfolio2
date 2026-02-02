721d33be6f1fca740a4f1bb747b46207e3beae1b
Ben Wolfram <benwolfram@Bens-MacBook-Air.local>
2026-02-01 22:19:43 -0600

    build fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Required for GitHub Pages: use your repo name; use '/' if using username.github.io
-  base: '/mesh-gradient/',
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
 })
